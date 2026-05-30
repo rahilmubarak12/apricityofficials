1019b4686215a3646db45f25388a9aac84b1015f
Rahil Mubarak <rizanmubarak@Rahils-Macbook.local>
2026-05-30 20:11:13 +0530

    feat: add Tawk.to live chat widget

--- hydrogen-store/app/root.tsx
+++ hydrogen-store/app/root.tsx
@@ -182,6 +182,22 @@ function Layout({children}: {children?: React.ReactNode}) {
           />
         )}
         <Scripts nonce={nonce} />
+        {/* Tawk.to Live Chat */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6a1af64433b64c1c2ac904a5/default';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
